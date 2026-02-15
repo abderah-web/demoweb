6c9c605c915aac28289abf769cc5edfd7267ccb0
abderah-web <abderrahim.lhaloui2003@gmail.com>
2026-02-15 02:28:10 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+function UpDate(previewPic) {
+    const x = document.getElementById("image");
+    x.style.backgroundImage = `url('${previewPic.src}')`;
+    x.style.backgroundSize = "cover";
+    x.style.backgroundPosition = "center";
+    x.innerHTML = previewPic.alt;
+}
+
+function unDo() {
+    const x = document.getElementById("image");
+    x.innerHTML = "Hover over a car below";
+    x.style.backgroundImage="";
+}
 
